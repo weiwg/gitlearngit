f6014f264ffa868d31d0f874bf6e78e7d29181f1
wei.gang1 <958493098@qq.com>
2022-07-07 10:40:02 +0800

    测试提交

---
+++ Admin.Core/LY.Report.Core.Services/Demo/Test/Input/DemoTestGetFenKuInput.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LY.Report.Core.Service.Demo.Test.Input
+{
+    /// <summary>
+    /// 查询
+    /// </summary>
+    public class DemoTestGetFenKuInput
+    {
+        /// <summary>
+        /// 班别
+        /// </summary>
+        [Display(Name="班别")]
+        [Required(ErrorMessage = "班别不能为空"), StringLength(20, ErrorMessage = "{0} 限制为{2}-{1} 个字符。", MinimumLength = 1)]
+        public string ClassAB { get; set; }
+
+        /// <summary>
+        /// 线别
+        /// </summary>
+        [Display(Name= "线别")]
+        [Required(ErrorMessage = "线别不能为空"), StringLength(20, ErrorMessage = "{0} 限制为{2}-{1} 个字符。", MinimumLength = 1)]
+        public string Line { get; set; }
+    }
+}
