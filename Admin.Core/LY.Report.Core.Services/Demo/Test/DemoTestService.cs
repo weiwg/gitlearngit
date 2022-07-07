86beeaf4758f745e1180a73a8b03df6b9e626c21
weig <958493098@qq.com>
2022-07-07 09:07:48 +0800

    修改接口dto参数校验

--- Admin.Core/LY.Report.Core.Services/Demo/Test/DemoTestService.cs
+++ Admin.Core/LY.Report.Core.Services/Demo/Test/DemoTestService.cs
@@ -139,12 +139,11 @@ namespace LY.Report.Core.Service.Demo.Test
         public async Task<IResponseOutput> GetPageListFenKuAsync(PageInput<DemoTestGetFenKuInput> input)
         {
             var proName = User.ProName;
-            DataTable dt1 = await _ib.Get(proName).Select<object>()
+            DataTable dt = await _ib.Get(proName).Select<object>()
             .WithSql(string.Format("select * from T_MES_Shift where ClassAB='{0}' and Line='{1}'", input.Filter?.ClassAB, input.Filter?.Line))
             .ToDataTableAsync("*");
 
-            return ResponseOutput.Data(dt1);
-            //return (Task<IResponseOutput>)ResponseOutput.Data(dt1);
+            return ResponseOutput.Data(dt);
         }
         #endregion
 
